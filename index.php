17492869757ccdbd0a759978fe07f642dd0a38e9
vinod <114590801+devopvinu@users.noreply.github.com>
2024-04-25 14:48:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
     $conn = mysqli_connect('db', 'jake', 'jake', "myDb");
     if ($conn){
-	    echo"<h1 style="color:green;"> Database Connected Sucessfully </h1>";
+	    echo"<h1 style='color:green;'> Database Connected Sucessfully </h1>";
     }else{
-	    echo"<h1 style="color:red;"> Unable To Connect To Database </h1>";
+	    echo"<h1 style='color:red;'> Unable To Connect To Database </h1>";
     }
 
 // Close connection
